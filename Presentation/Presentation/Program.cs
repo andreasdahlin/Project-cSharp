69dfd4bae814351ea79c5e747fb0ab30d528d938
JesperBergmark <jesperbergmark86@gmail.com>
2017-10-18 13:37:58 +0200

    Xml länk fixad

--- Presentation/Presentation/Program.cs
+++ Presentation/Presentation/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Windows.Forms;
 
 namespace Presentation
@@ -17,6 +18,23 @@ namespace Presentation
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainInterfaceForm());
+
+            try
+            {
+                var xmlDoc = new XmlDocument();
+                const string file = @"http://podcasts.joerogan.net/feed";
+                xmlDoc.Load(file);
+                xmlDoc.Save(Console.Out);
+                Console.Read();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(@"The file could not be read.");
+                Console.WriteLine(e.Message);
+                
+            }
+            Console.Read();
         }
     }
 }
