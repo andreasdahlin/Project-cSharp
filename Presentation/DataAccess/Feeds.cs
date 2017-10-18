d98307e7e1afafa2839d1ea8fc8db49faca57ad2
andreasdahlin <andreasdahlin@live.se>
2017-10-18 10:40:08 +0200

    Fixade fält, konstructor och propreties i klassen Feeds

--- Presentation/DataAccess/Feeds.cs
+++ Presentation/DataAccess/Feeds.cs
@@ -8,5 +8,23 @@ namespace DataAccess
 {
     class Feeds
     {
+        private string url;
+        private string name;
+        private int interval;
+        private Categories categoryName;
+
+        public Feeds(string url, string name, int interval, Categories categoryName)
+        {
+            this.Url = url;
+            this.Name = name;
+            this.Interval = interval;
+            this.CategoryName = categoryName;
+
+        }
+
+        public string Url { get => url; set => url = value; }
+        public string Name { get => name; set => name = value; }
+        public int Interval { get => interval; set => interval = value; }
+        internal Categories CategoryName { get => categoryName; set => categoryName = value; }
     }
 }
